90a2320aeb2ea85e81c325e4b6c2703baefe37e1
Courtney828 <courtneyserongwa@gmail.com>
2020-08-20 17:50:42 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -5,30 +5,34 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-let Students = [{StudentName:"", StudentSuname:"",StudentAge:"",  StudentGrade:""},
-            {StudentName:"Courtney", StudentSuname:"Serongwa",StudentAge:13,StudentGrade:"G7"},
-              {StudentName:"candy", StudentSuname:"Vile",StudentAge:14,StudentGrade:"G8"},
-              {StudentName:"Palesa", StudentSuname:"Maja",StudentAge:15,StudentGrade:"G9"},
-              {StudentName:"Given", StudentSuname:"Bile",StudentAge:10,StudentGrade:"G7"},
-              {StudentName:"Tondi", StudentSuname:"Murwa",StudentAge:9,StudentGrade:"G6"},
-              {StudentName:"James", StudentSuname:"Mohammed",StudentAge:11,StudentGrade:"G5"},
-              {StudentName:"Keletso", StudentSuname:"Jab",StudentAge:18,StudentGrade:"G12"},
-              {StudentName:"Ben", StudentSuname:"Kunutu",StudentAge:20,StudentGrade:"G12"
+let Students = [{StudentName:"", StudentSurname:"",StudentAge:"",  StudentGrade:""},
+            {StudentName:"Courtney", StudentSurname:"Serongwa",StudentAge:13,StudentGrade:"G7"},
+              {StudentName:"candy", StudentSurname:"Vile",StudentAge:14,StudentGrade:"G8"},
+              {StudentName:"Palesa", StudentSurname:"Maja",StudentAge:15,StudentGrade:"G9"},
+              {StudentName:"Given", StudentSurname:"Bile",StudentAge:10,StudentGrade:"G7"},
+              {StudentName:"Tondi", StudentSurname:"Murwa",StudentAge:9,StudentGrade:"G6"},
+              {StudentName:"James", StudentSurname:"Mohammed",StudentAge:11,StudentGrade:"G5"},
+              {StudentName:"Keletso", StudentSurname:"Jab",StudentAge:18,StudentGrade:"G12"},
+              {StudentName:"Ben", StudentSurname:"Kunutu",StudentAge:20,StudentGrade:"G12"
               }]
   
 
 
 let max=40;  
 var highest =0; 
-var name="ben";
-var surname="Kunutu";
+var name;
+var surname;
 
 for(var x= 0; x<Students.length; x++){
   if (Students[x].StudentAge<max){
      highest =Students[x].StudentAge;
-    
+name=Students[x].StudentName;
+surname=Students[x].StudentSurname;
   }
 }
 
   console.log(name +" "+ surname+ " " + highest)  ;
+
+
+
   
