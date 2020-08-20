2585459221ea976f5134ecf3bc5627c0f78cf6eb
Courtney828 <courtneyserongwa@gmail.com>
2020-08-20 15:11:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-1tbiru
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-1tbiru)
---
+++ index.ts
@@ -0,0 +1,30 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+let Students = [{StudentName:"", StudentSuname:"",StudentAge:"",  StudentGrade:""},
+            {StudentName:"Courtney", StudentSuname:"Serongwa",StudentAge:13,StudentGrade:"G7"},
+              {StudentName:"candy", StudentSuname:"Vile",StudentAge:14,StudentGrade:"G8"},
+              {StudentName:"Palesa", StudentSuname:"Maja",StudentAge:15,StudentGrade:"G9"},
+              {StudentName:"Given", StudentSuname:"Bile",StudentAge:10,StudentGrade:"G7"},
+              {StudentName:"Tondi", StudentSuname:"Murwa",StudentAge:9,StudentGrade:"G6"},
+              {StudentName:"James", StudentSuname:"Mohammed",StudentAge:11,StudentGrade:"G5"},
+              {StudentName:"Keletso", StudentSuname:"Jab",StudentAge:18,StudentGrade:"G12"},
+              {StudentName:"Ben", StudentSuname:"Kunutu",StudentAge:20,StudentGrade:"G12"
+              }]
+      let max=0;  
+    
+function OldStudent (Students) {
+  if(Students.StudentAge > max)
+		return max;
+
+ console.log("The Oldest student is"+ Students); 
+}
+
+
+
+  
+  
